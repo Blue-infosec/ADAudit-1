dd59ddf9df3adfd0880719bb2336ba64c718e769
Carlos Perez <carlos_perez@darkoperator.com>
2016-03-16 21:23:15 -0400

    initial set of commits

---
+++ Get-DSForest.Tests.ps1
@@ -0,0 +1,20 @@
+﻿Param(
+    [pscredentia]$Credential,
+    [string]$ComputerName,
+    [string]$Forest
+)
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "GetCurrentForest" {
+    if ((!$ComputerName) -and (!$Credential) -and (!$Forest))
+    {
+        It "Get current machine forest" {
+            (Get-DSForest).GetType().FullName | Should Be 'System.DirectoryServices.ActiveDirectory.Forest'
+        }
+    }
+}
+
+
---
+++ Get-DSForest.ps1
@@ -0,0 +1,63 @@
+﻿function Get-DSForest {
+    [CmdletBinding(DefaultParameterSetName = 'Current')]
+    param(
+        # Domain controller to connect to when not in a domain.
+        [Parameter(ParameterSetName = 'Remote',
+                   Mandatory = $true)]
+        [string]
+        $ComputerName,
+        
+        # Credentials to use for getting forest information.
+        [Parameter(ParameterSetName = 'Remote',
+                   Mandatory = $true)]
+        [Parameter(ParameterSetName = 'OtherForest',
+                    Mandatory = $false)]
+        [pscredential]
+        $Credential,
+        
+        # Forest name.
+        [Parameter(ParameterSetName = 'OtherForest',
+                   Mandatory = $true)]
+        [string]
+        $ForestName
+    )
+    
+    begin {
+    }
+    
+    process {
+        $sig = @"
+[DllImport("Netapi32.dll", CharSet=CharSet.Unicode, SetLastError=true)]
+public static extern int NetGetJoinInformation(string server,out IntPtr domain,out int status);
+"@
+
+        switch ($PSCmdlet.ParameterSetName) {
+            'Current' {
+                $type = Add-Type -MemberDefinition $sig -Name Win32Utils -Namespace NetGetJoinInformation -PassThru
+                $ptr = [IntPtr]::Zero
+                $joinstatus = 0
+                $type::NetGetJoinInformation($null, [ref] $ptr, [ref]$joinstatus) |Out-Null
+                
+                if ($joinstatus -eq 3){
+                    $ForestObject = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()    
+                } else {
+                    throw 'This computer is not joined to a domain so no forest could be retrieved.'
+                }
+            }
+            
+            'Remote' {
+                
+            }
+            
+            'OtherForest' {
+                
+            }
+            Default {}
+        }
+        $ForestObject
+    }
+    
+    end {
+    }
+}
+
