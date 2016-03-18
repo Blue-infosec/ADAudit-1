f33289ce401c6e6c3bbeaeb1b95806ad8ca777ef
Carlos Perez <carlos_perez@darkoperator.com>
2016-03-17 22:11:20 -0400

    Second test

--- Get-DSForest.Tests.ps1
+++ Get-DSForest.Tests.ps1
@@ -26,6 +26,8 @@ Describe "Get-DSForest" {
     $ptr = [IntPtr]::Zero
     $joinstatus = 0
     $type::NetGetJoinInformation($null, [ref] $ptr, [ref]$joinstatus) |Out-Null
+    
+    # Test for getting the forrest for which the machine is a member of.
     if ($joinstatus -eq 3)
     {
         It "Get current machine forest" {
@@ -35,6 +37,14 @@ Describe "Get-DSForest" {
         It 'Fails to get forest because host is not domain joined' {
             {Get-DSForest} | Should Throw 'This computer is not joined to a domain so no forest could be retrieved.'
         }
+    }
+    
+    # Test by connecting to remote domain controller to get the forest it is a member off.
+    if ($ComputerName.length -gt 0 -and $Credential.Username -ne $null) {
+        It 'Get remote forest'{
+             $ForestObject = Get-DSForest -ComputerName $ComputerName -Credential $Credential
+             {$ForestObject -is [System.DirectoryServices.ActiveDirectory.Forest]} | Should Be $true
+        }   
     }
 }
 
