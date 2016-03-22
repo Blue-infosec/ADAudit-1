8c50111d0c768cb0d9b438d08828bad5134067fa
Carlos Perez <carlos_perez@darkoperator.com>
2016-03-22 13:46:30 -0400

    Update Get-DSForest to pull SID

--- Get-DSForest.ps1
+++ Get-DSForest.ps1
@@ -9,23 +9,23 @@
 .PARAMETER Credential
     Alternate credentials for retrieving forest information.
 .PARAMETER Forest
-    Fully Qualified Name of a forest to get information on when running from a host joined 
+    Fully Qualified Name of a forest to get information on when running from a host joined
     to the domain.
 .EXAMPLE
     C:\PS> Get-DSForest
     Gets the forest for the domain the host is corrently joined to.
 .EXAMPLE
     C:\PS> Get-DSForest -ComputerName dc01.acmelabs.com -Credential (Get-Credential user1)
-    Connect to a remote domain controller and get the forest for domain it manages using the 
+    Connect to a remote domain controller and get the forest for domain it manages using the
     provided credentials.
 .EXAMPLE
-    C:\PS> Get-DSForest -ForestName frabrikan.com 
+    C:\PS> Get-DSForest -ForestName frabrikan.com
     Gets the forest frabikan.com that the current domain has a trust relationship with.
 .OUTPUTS
     System.DirectoryServices.ActiveDirectory.Forest
 .NOTES
-    This function is heavily dependent on DNS. The host running the function is highly 
-    recomended to be using the same DNS server as the domain whe are querying.  
+    This function is heavily dependent on DNS. The host running the function is highly
+    recomended to be using the same DNS server as the domain whe are querying.
 #>
 function Get-DSForest {
     [CmdletBinding(DefaultParameterSetName = 'Current')]
@@ -35,7 +35,7 @@ function Get-DSForest {
                    Mandatory = $true)]
         [string]
         $ComputerName,
-        
+
         # Credentials to use for getting forest information.
         [Parameter(ParameterSetName = 'OtherForest',
                     Mandatory = $false)]
@@ -44,17 +44,17 @@ function Get-DSForest {
         [Management.Automation.PSCredential]
         [Management.Automation.CredentialAttribute()]
         $Credential = [Management.Automation.PSCredential]::Empty,
-        
+
         # Forest name.
         [Parameter(ParameterSetName = 'OtherForest',
                    Mandatory = $true)]
         [string]
         $ForestName
     )
-    
+
     begin {
     }
-    
+
     process {
         $sig = @"
 [DllImport("Netapi32.dll", CharSet=CharSet.Unicode, SetLastError=true)]
@@ -67,14 +67,20 @@ public static extern int NetGetJoinInformation(string server,out IntPtr domain,o
                 $ptr = [IntPtr]::Zero
                 $joinstatus = 0
                 $type::NetGetJoinInformation($null, [ref] $ptr, [ref]$joinstatus) |Out-Null
-                
+
                 if ($joinstatus -eq 3){
-                    $ForestObject = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()    
+                    $ForestObject = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+                    if ($ForestObject -ne $null) {
+                        $RootDN = "DC=$(($ForestObject.Name).replace('.',',DC='))"
+                        $DEObj = Get-DSDirectoryEntry -DistinguishedName $RootDN
+                        $Sid = (New-Object -TypeName System.Security.Principal.SecurityIdentifier($DEObj.objectSid.value,0)).value
+                        Add-Member -InputObject $ForestObject -MemberType NoteProperty -Name 'Sid' -Value $Sid
+                    }
                 } else {
                     throw 'This computer is not joined to a domain so no forest could be retrieved.'
                 }
             }
-            
+
             'Remote' {
                 $cArgs = @(
                     'DirectoryServer',
@@ -85,8 +91,14 @@ public static extern int NetGetJoinInformation(string server,out IntPtr domain,o
                 $typeName = 'DirectoryServices.ActiveDirectory.DirectoryContext'
                 $context = New-Object $typeName  $cArgs
                 $ForestObject = [DirectoryServices.ActiveDirectory.Forest]::GetForest($context)
+                if ($ForestObject -ne $null) {
+                        $RootDN = "DC=$(($ForestObject.Name).replace('.',',DC='))"
+                        $DEObj = Get-DSDirectoryEntry -DistinguishedName $RootDN -ComputerName $ComputerName -Credential $Credential
+                        $Sid = (New-Object -TypeName System.Security.Principal.SecurityIdentifier($DEObj.objectSid.value,0)).value
+                        Add-Member -InputObject $ForestObject -MemberType NoteProperty -Name 'Sid' -Value $Sid
+                    }
             }
-            
+
             'OtherForest' {
                 if ($Credential.UserName -ne $null){
                     # Arguments to get forest with alternate credentials
@@ -106,12 +118,22 @@ public static extern int NetGetJoinInformation(string server,out IntPtr domain,o
                 $typeName = 'DirectoryServices.ActiveDirectory.DirectoryContext'
                 $context = New-Object $typeName  $cArgs
                 $ForestObject = [DirectoryServices.ActiveDirectory.Forest]::GetForest($context)
+                if ($ForestObject -ne $null) {
+                        $RootDN = "DC=$(($ForestObject.Name).replace('.',',DC='))"
+                        if ($Credential.UserName -ne $null){
+                            $DEObj = Get-DSDirectoryEntry -DistinguishedName $RootDN -Credential $Credential
+                        } else {
+                            $DEObj = Get-DSDirectoryEntry -DistinguishedName $RootDN
+                        }
+                        $Sid = (New-Object -TypeName System.Security.Principal.SecurityIdentifier($DEObj.objectSid.value,0)).value
+                        Add-Member -InputObject $ForestObject -MemberType NoteProperty -Name 'Sid' -Value $Sid
+                    }
             }
             Default {}
         }
         $ForestObject
     }
-    
+
     end {
     }
 }
